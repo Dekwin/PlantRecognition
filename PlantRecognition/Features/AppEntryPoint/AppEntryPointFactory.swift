b7cc3f6e70504fa669dbd865bb06e375fa849ebd
Igor Kasyanenko <dekstersniper@gmail.com>
2021-05-23 16:47:45 +0300

    removes storyboard

---
+++ PlantRecognition/Features/AppEntryPoint/AppEntryPointFactory.swift
@@ -0,0 +1,15 @@
+//
+//  AppEntryPointFactory.swift
+//  PlantRecognition
+//
+//  Created by Igor Kasyanenko on 23.05.2021.
+//
+
+import Foundation
+import UIKit
+
+final class AppEntryPointFactory {
+    func create() -> UIViewController {
+        return UIViewController()
+    }
+}
